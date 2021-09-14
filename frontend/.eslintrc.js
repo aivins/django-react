a57f3f02f32673e5fe9c7943d493b4d790040c54
Andrew Ivins <andrew@ivins.id.au>
2021-09-14 11:23:17 +0800

    Add prettier-eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,21 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "relay"],
+  rules: {
+    "relay/graphql-syntax": "error",
+    "relay/compat-uses-vars": "warn",
+    "relay/graphql-naming": "error",
+    "relay/generated-flow-types": "warn",
+    "relay/must-colocate-fragment-spreads": "warn",
+    "relay/no-future-added-value": "warn",
+    "relay/unused-fields": "warn",
+    "relay/function-required-argument": "warn",
+    "relay/hook-required-argument": "warn",
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
